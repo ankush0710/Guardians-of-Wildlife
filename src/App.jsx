afc80625c3f23b569f51def55eb94c4e8754103e
ankush0710 <ankushkurvey053@gmail.com>
2026-02-20 19:22:24 +0530

    added title and navlinks to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faInstagram, faFacebook, faTwitter, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import Navbar from "./Components/Navbar";
 
 function App() {
-
+  library.add(faInstagram, faFacebook, faTwitter, faLinkedin);
   return (
     <>
       <Navbar />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,67 @@
 //here I am created responsive navbar and all routes and link
-
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { HiMiniBars2 } from "react-icons/hi2";
 
 const Navbar = () => {
-    return(
-        <>
-        <div id="navbar" className='text-black font-xl bg-red-300'>
-            <h1>Navbar</h1>
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <nav className="bg-[#111F35] w-80 py-3 px-6 rounded-full mx-auto my-10 md:w-2xl md:px-10">
+        <div id="navbar" className="flex justify-between items-center">
+          <div
+            id="nav-links-left"
+            className="hidden lg:flex items-center gap-4"
+          >
+            <Link to="/">
+              <p className="font: font-body text-white text-sm">HOME</p>
+            </Link>
+            <Link to="/Program">
+              <p className="font: font-body text-white text-sm">PROGRAM</p>
+            </Link>
+          </div>
+          <Link to="/">
+            <div id="nav-logo" className="flex flex-col justify-center">
+              <h1 className="font: font-heading font-semibold text-white text-xl text-center">
+                Guardians Of Wildlife.
+              </h1>
+              <p className="font: font-heading font-medium text-white text-xs text-center">
+                Protect Wildlife, Preserve Life
+              </p>
+            </div>
+          </Link>
+          <div
+            id="navlinks-right"
+            className="hidden lg:flex items-center gap-4"
+          >
+            <Link to="/JoinOurTeam">
+              <p className="font: font-body text-white text-sm">
+                JOIN OUT TEAM
+              </p>
+            </Link>
+            <Link to="/ContactUs">
+              <p className="font: font-body text-white text-sm">CONTACT US</p>
+            </Link>
+          </div>
+          <button
+            className="lg:hidden text-white"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            <HiMiniBars2 size={30} />
+          </button>
         </div>
-        </>
-    )
-}
+      </nav>
+
+      {/* For mobile view hammburger button will display*/}
+      <div>
+        
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
