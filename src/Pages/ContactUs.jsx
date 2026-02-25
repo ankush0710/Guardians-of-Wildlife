2409df62048a75d6a3274659b8918b206782bd7d
ankush0710 <ankushkurvey053@gmail.com>
2026-02-25 17:20:41 +0530

    some changes done in contact form

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -6,13 +6,14 @@ const ContactUs = () => {
   return (
     <>
       <div id="container" className="md:flex md:justify-between">
-
         {/* address of offices in INDIA and LA  */}
         <div
           id="address_section"
-          className="flex flex-col gap-10 mt-10 md:flex-1 md:ms-8"
-        >  
-          <h1 className="font-heading text-3xl font-bold text-center">Find Us</h1>
+          className="flex flex-col gap-10 mx-3 mt-10 md:flex-1 md:ms-8"
+        >
+          <h1 className="font-heading text-3xl font-bold text-center">
+            Find Us
+          </h1>
           <div
             id="card-1"
             className="h-lg max-w-xl bg-[#ECE7D1] rounded-xl shadow-lg p-4"
@@ -28,6 +29,17 @@ const ContactUs = () => {
               Contact -
               <span className="hover:border-b-2 hover:p-1 hover:cursor-pointer">
                 +91-1234567890
+              </span>
+              .
+            </p>
+            <p className="font-body text-md leading-8">
+              Location -
+              <span className="px-1 hover:border-b-2 hover:p-1 hover:cursor-pointer">
+                12.9722&deg;N
+              </span>
+              ,
+              <span className="px-1 hover:border-b-2 hover:p-1 hover:cursor-pointer">
+                79.5956&deg;E
               </span>
               .
             </p>
@@ -50,6 +62,17 @@ const ContactUs = () => {
               </span>
               .
             </p>
+            <p className="font-body text-md leading-8">
+              Location -
+              <span className="px-1 hover:border-b-2 hover:p-1 hover:cursor-pointer">
+                12.9722&deg;N
+              </span>
+              ,
+              <span className="px-1 hover:border-b-2 hover:p-1 hover:cursor-pointer">
+                79.5956&deg;E
+              </span>
+              .
+            </p>
           </div>
         </div>
 
@@ -60,45 +83,46 @@ const ContactUs = () => {
           </h1>
           <div
             id="contact-form"
-            className="bg-[#ECE7D1] rounded-xl flex flex-col pt-10 pb-15 px-10 mx-3 my-10 relative md:w-auto lg:w-2xl md:shadow-lg"
+            className="bg-[#ECE7D1] rounded-xl flex flex-col py-15 px-10 mx-3 my-10 relative md:w-auto lg:w-2xl md:shadow-lg"
           >
             <form action="">
-              {/* input box for first name  */}
-              <div className="flex gap-4">
-                <div className="relative z-0 w-full mb-5 group flex-1">
-                <input
-                  type="text"
-                  name="first_name"
-                  id="first_name"
-                  className="block py-2.5 px-0 w-full text-md bg-transparent border-0 border-b-2 border-default-medium border-[#8A7650] appearance-none focus:outline-none focus:ring-0 focus:border-[#562F00] peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="first_name"
-                  className="absolute font-body text-md text-[#8A7650] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
-                >
-                  First Name
-                </label>
-              </div>
+              <div className="md:flex md:gap-4">
 
-              {/* input box for last name  */}
-              <div className="relative z-0 w-full mb-5 group flex-1">
-                <input
-                  type="text"
-                  name="last_name"
-                  id="last_name"
-                  className="block py-2.5 px-0 w-full text-md bg-transparent border-0 border-b-2 border-default-medium border-[#8A7650] appearance-none focus:outline-none focus:ring-0 focus:border-[#562F00] peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="last_name"
-                  className="absolute font-body text-md text-[#8A7650] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
-                >
-                  Last Name
-                </label>
-              </div>
+                {/* input box for first name  */}
+                <div className="relative z-0 w-full mb-5 group md:flex-1">
+                  <input
+                    type="text"
+                    name="first_name"
+                    id="first_name"
+                    className="block py-2.5 px-0 w-full text-md bg-transparent border-0 border-b-2 border-default-medium border-[#8A7650] appearance-none focus:outline-none focus:ring-0 focus:border-[#562F00] peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    for="first_name"
+                    className="absolute font-body text-md text-[#8A7650] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                  >
+                    First Name
+                  </label>
+                </div>
+
+                {/* input box for last name  */}
+                <div className="relative z-0 w-full mb-5 group md:flex-1">
+                  <input
+                    type="text"
+                    name="last_name"
+                    id="last_name"
+                    className="block py-2.5 px-0 w-full text-md bg-transparent border-0 border-b-2 border-default-medium border-[#8A7650] appearance-none focus:outline-none focus:ring-0 focus:border-[#562F00] peer"
+                    placeholder=" "
+                    required
+                  />
+                  <label
+                    for="last_name"
+                    className="absolute font-body text-md text-[#8A7650] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-fg-brand peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+                  >
+                    Last Name
+                  </label>
+                </div>
               </div>
 
               {/* input box for email  */}
