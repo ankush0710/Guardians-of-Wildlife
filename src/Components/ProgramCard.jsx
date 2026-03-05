e429c05834776351e014a33d7615a8de49b11485
ankush0710 <ankushkurvey053@gmail.com>
2026-03-05 19:41:09 +0530

    some UI changes in cards

--- src/Components/ProgramCard.jsx
+++ src/Components/ProgramCard.jsx
@@ -5,15 +5,26 @@ import sample from "../assets/sample-image.jpg";
 const ProgramCard = () => {
   return (
     <>
-    <div id="program-card" className="relative h-auto mx-5 rounded-xl flex flex-col gap-5 items-center bg-cover bg-center md:bg-none" style={{backgroundImage: `url(${sample})`}}>
-        {/* <div id="card-image" className="relative w-78 h-78 border border-gray-500 mt-10 ">
-          <img src={sample} alt="image" className="w-full h-full object-contain"/>
-        </div> */}
-        <div className="absolute inset-0 bg-black opacity-25"></div>
-        
-        <div id="description" className="relative px-5 py-10">
-          <p className="font-body text-white">
-            <span className="font-heading  text-xl font-bold space-x-2">Description:- </span>Project Tiger was launched in 1973 with the aim of conserving Bengal
+      <div
+        id="program-card"
+        className="w-72 h-auto mx-3 p-2 rounded-lg flex flex-col gap-5 items-center border border-gray-300 bg-[#ECE7D1] md:w-auto md:h-2xl md:flex md:flex-row md:items-start"
+      >
+        <div
+          id="card-image"
+          className="w-full h-70 md:h-50 md:w-xl"
+        >
+          <img
+            src={sample}
+            alt="image"
+            className="w-full h-full object-cover rounded-lg"
+          />
+        </div>
+
+        {/* heading and tthe main description about the project  */}
+        <div id="description" className="space-y-3 md:pt-5">
+          <h1 className="font-heading font-bold text-2xl">Project Tiger</h1>
+          <p className="font-body text-sm">
+            Project Tiger was launched in 1973 with the aim of conserving Bengal
             tigers. This program established over 50 tiger reserves across India
             to safeguard tigers and their natural habitats. Efforts include
             anti-poaching strategies, habitat restoration, and enhancing the
@@ -24,12 +35,14 @@ const ProgramCard = () => {
             broader conservation model.
           </p>
         </div>
-        <div className="relative pb-10">
+
+        {/* button to see full detailed article of the respective project  */}
+        <div className="pb-10">
           <button
             type="submit"
             className="text-[#8A7650] font-semibold font-body bg-transparent border-2 border-[#8A7650] px-6 py-2 rounded-full group hover:text-[#562F00] hover:bg-[#8A7650] hover:border-2 hover:border-[#562F00] hover:duration-600"
           >
-            view article
+            view details
             <span>
               <FontAwesomeIcon
                 icon="fa-solid fa-arrow-down"
