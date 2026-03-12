3f3f18fb5fd35d57bcd5149f510170c5d98a1a1e
ankush0710 <ankushkurvey053@gmail.com>
2026-03-12 19:07:54 +0530

    search functionality added to wildlife component

--- src/Pages/Wildlife.jsx
+++ src/Pages/Wildlife.jsx
@@ -9,37 +9,47 @@ const Wildlife = () => {
   const WildlifeData = useSelector((state) => state.WildlifeData);
   const [currentPage, setCurrentPage] = useState(1);
   const [cardPerPage, setCardPerPage] = useState(9);
+  const [search, setSearch] = useState("");
+  const [filteredSearch, setFilteredSearch] = useState("");
+
 
   useEffect(() => {
     dispatch(FetchWildlifeData());
   }, [WildlifeData]);
 
+  // search logic for search data by name
+  const filterName = useMemo(() => {
+    return WildlifeData.filter((wd) =>
+      wd.name.toLowerCase().includes(filteredSearch.toLowerCase()),
+    );
+  }, [filteredSearch]);
+
+  const handleSearch = () => {
+    setFilteredSearch(search);
+    setCurrentPage(1);
+  };
+
   //method for displaying 8 cards in md devices and 9 cards in lg devices
-  useEffect(()=>{
-    if(window.innerWidth >= 1024){
+  useEffect(() => {
+    if (window.innerWidth >= 1024) {
       setCardPerPage(9);
-    }
-    else if(window.innerWidth >= 768){
+    } else if (window.innerWidth >= 768) {
       setCardPerPage(8);
     }
-  })
+  });
 
   //pagination logic start here
   const currentCard = useMemo(() => {
     const lastIndex = currentPage * cardPerPage;
     const startIndex = lastIndex - cardPerPage;
-    return WildlifeData.slice(startIndex, lastIndex);
-  }, [currentPage, cardPerPage]);
+    return filterName.slice(startIndex, lastIndex);
+  }, [currentPage, cardPerPage, filterName]);
 
-  // total number of pages will display 
-  const TotalPage = Math.ceil(WildlifeData.length / cardPerPage);
+  // total number of pages will display
+  const TotalPage = Math.ceil(filterName.length / cardPerPage);
 
   const selectPageHandle = (selected) => {
-    if (
-      selected >= 1 &&
-      selected !== currentPage &&
-      selected <= TotalPage
-    ) {
+    if (selected >= 1 && selected !== currentPage && selected <= TotalPage) {
       setCurrentPage(selected);
     }
   };
@@ -54,8 +64,82 @@ const Wildlife = () => {
       </div>
     );
   }
+
   return (
     <>
+      {/* //search bar and filter button */}
+      <div className="mx-10 text-center md: flex md:justify-between md:items-center md:gap-4">
+        <div className="relative">
+          <input
+            type="text"
+            value={search}
+            placeholder="Search by name"
+            className="py-2 px-15 border border-gray-500 rounded-full bg-gray-300 focus:border-gray-600"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button
+            onClick={handleSearch}
+            className="absolute inset-y-0 right-0 px-4 rounded-e-full border border-gray-500 bg-blue-500 text-white font-body font-semibold hover:bg-blue-800 cursor-pointer"
+          >
+            Search
+          </button>
+        </div>
+        {/* <el-dropdown class="inline-block">
+          <button class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-gray-300 px-3 py-2 text-sm font-semibold text-black inset-ring-1 inset-ring-white/5 hover:bg-gray-400">
+            Filter by Status
+            <svg
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              data-slot="icon"
+              aria-hidden="true"
+              class="-mr-1 size-5 text-gray-400"
+            >
+              <path
+                d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+              />
+            </svg>
+          </button>
+
+          <el-menu
+            anchor="bottom end"
+            popover
+            class="w-56 origin-top-right rounded-md bg-gray-800 outline-1 -outline-offset-1 outline-white/10 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+          >
+            <div class="py-1">
+              <a
+                href="#"
+                class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:text-white focus:outline-hidden"
+              >
+                Account settings
+              </a>
+              <a
+                href="#"
+                class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:text-white focus:outline-hidden"
+              >
+                Support
+              </a>
+              <a
+                href="#"
+                class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:text-white focus:outline-hidden"
+              >
+                License
+              </a>
+              <form action="#" method="POST">
+                <button
+                  type="submit"
+                  class="block w-full px-4 py-2 text-left text-sm text-gray-300 focus:bg-white/5 focus:text-white focus:outline-hidden"
+                >
+                  Sign out
+                </button>
+              </form>
+            </div>
+          </el-menu>
+        </el-dropdown> */}
+      </div>
+
+      {/* main content sectio  */}
       <div className="w-full h-auto my-10 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {currentCard.map((Wdata) => {
           return (
@@ -70,7 +154,7 @@ const Wildlife = () => {
               </div>
               <div className="relative z-10 p-6 space-y-1 group-hover:text-hidden">
                 <h1 className="text-white font-heading text-xl">
-                  Name :{" "}
+                  Name :
                   <span className="font-body text-lg ps-5">{Wdata.name}</span>
                 </h1>
                 <h1 className="text-white font-heading text-xl">
@@ -132,7 +216,7 @@ const Wildlife = () => {
               onClick={() => selectPageHandle(currentPage - 1)}
             />
           </span>
-          {[...Array.from({length: TotalPage})].map((_, i) => {
+          {[...Array.from({ length: TotalPage })].map((_, i) => {
             return (
               <span
                 key={i}
