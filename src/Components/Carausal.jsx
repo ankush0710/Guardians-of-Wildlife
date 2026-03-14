69e4175cc07e32ce04b644c452c767a1c674a466
ankush0710 <ankushkurvey053@gmail.com>
2026-03-14 19:48:29 +0530

    fix issues with carousal

--- src/Components/Carausal.jsx
+++ src/Components/Carausal.jsx
@@ -1,21 +1,87 @@
-import React from 'react';
+import React, { useState } from "react";
+import Cards from "./Cards";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Carausal = ({carousalData}) => {
-    return(
-        <>
-        <div className="w-full mx-auto overflow-hidden relative">
-            <div className=''>
-            {carousalData.map((d, index) => {
-                return(
-                    <div className='w-full'>
-                        <img key={index} src={d.image} alt="sample-image" className='w-full object-cover flex-shrink-0'/>
-                    </div>
-                )
-            })}
-        </div>
-        </div>
-        </>
-    )
-}
+const Carausal = ({ carousalData }) => {
+  const [current, setCurrent] = useState(0);
 
-export default Carausal
+  // function for go to previous slide
+  const prevSlide = () => {
+    if (current == 0) {
+      setCurrent(carousalData.length - 1);
+    } else {
+      setCurrent(current - 1);
+    }
+  };
+
+  // function for go to next slide
+  const nextSlide = () => {
+    if (current == carousalData.length - 1) {
+      setCurrent(0);
+    } else {
+      setCurrent(current + 1);
+    }
+  };
+  return (
+    <>
+      <div className="overflow-hidden relative w-full">
+        <div
+          className={`flex transition-transform ease-in-out duration-400`}
+          style={{
+            transform: `translateX(-${current * 100}%)`,
+          }}
+        >
+          {carousalData.map((d) => {
+            return (
+              <>
+                <div
+                  key={d.id}
+                  className="w-full flex-shrink-0 relative h-[350px] md:h-[450px] lg:h-[550px]"
+                >
+                  <img
+                    src={d.image}
+                    alt="image"
+                    className="w-full h-full object-cover"
+                  />
+                  <div className="bg-black absolute inset-0 opacity-50"></div>
+                  <div
+                    id="card"
+                    className="relative w-full md:w-[90%] lg:w-[420px] bg-[#EBF4F6] p-5 border border-gray-400 rounded-lg shadow-xl mt-4 mx-auto lg:absolute lg:bottom-20 lg:left-10"
+                  >
+                    <p className="font-body text-lg text-gray-600 py-2 md:text-center">
+                      <FontAwesomeIcon
+                        icon="fa-solid fa-quote-right"
+                        className="ps-3 pb-1 rotate-y-180"
+                      />
+                      {d.message}
+                      <FontAwesomeIcon
+                        icon="fa-solid fa-quote-right"
+                        className="ps-3 pb-1"
+                      />
+                    </p>
+                    <p className="text-gray-500 font-body font-semibold text-lg text-right pt-10">
+                      ~ {d.name}
+                    </p>
+                    <p className="text-gray-500  font-body font-semibold text-sm text-end pt-1">
+                      - {d.position}
+                    </p>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+        <div className="absolute top-0 h-full w-full flex justify-between items-center text-white px-5">
+          <button onClick={prevSlide} className="text-xl">
+            <FontAwesomeIcon icon="fa-solid fa-angle-left" />
+          </button>
+          <button onClick={nextSlide} className="text-xl hover:text-2xl">
+            <FontAwesomeIcon icon="fa-solid fa-angle-right" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Carausal;
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -27,9 +27,8 @@ const Dashboard = () => {
   const carousalData = [
     {
       id: 1,
-      image:
-        "https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      name: "Dr. Saurabh Sharma",
+      image:"https://img.freepik.com/free-photo/confident-young-man-looking-determined-cross-arms-chest-wearing-orange-winter-sweater-standing-ag_1258-155306.jpg?semt=ais_rp_progressive&w=740&q=80",
+      name: "Saurabh Sharma",
       position: "CEO, Founder of Gaurdians of Wildlife",
       message:
         "Wildlife conservation is essential to maintain ecosystem balance, protect biodiversity, and ensure a healthy planet for future generations by safeguarding habitats and endangered species. The core message is to protect natural habitats from destruction, combat species reduction caused by humans, and recognize that wildlife is a priceless treasure",
@@ -37,8 +36,8 @@ const Dashboard = () => {
     {
       id: 2,
       image:
-        "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      name: "Abhinav Thakur",
+        "https://as2.ftcdn.net/jpg/02/88/33/79/1000_F_288337966_NdtrgYYHMgPxvCu3mEPZZKuKecxVpXA2.jpg",
+      name: "Dr. Abhinav Thakur",
       position: "Marketing Head, Co-founder of Gaurdians of Wildlife",
       message:
         "The human population has grown exponentially over the past 200 years, to more than eight billion humans as of November 2022, and it continues to rapidly grow. This means natural resources are being consumed faster than ever by the billions of people on the planet. This growth and development also endangers the habitats and existence of various types of wildlife around the world, particularly animals and plants that may be displaced for land development, used for food or other human purposes. Other threats to wildlife include the introduction of invasive species from other parts of the world, climate change, pollution, hunting, fishing and poaching.",
@@ -74,7 +73,7 @@ const Dashboard = () => {
       </div>
 
       {/* introduction of the organzation section*/}
-      <h1 className="my-10 text-5xl font-heading text-center font-bold text-gray-500">
+      <h1 className="my-2 text-3xl font-heading text-center font-bold text-gray-500 md:text-5xl md:my-5">
         {dashboardData[0].title}
       </h1>
       <div className="my-5 md:flex md:justify-between">
@@ -95,7 +94,7 @@ const Dashboard = () => {
       <div className="border-1 border-gray-400 m-5"></div>
 
       {/* this section describe about the work we are doing */}
-      <h1 className="mt-10 text-5xl font-heading text-center font-bold text-gray-500 ">
+      <h1 className="mt-10 text-3xl font-heading text-center font-bold text-gray-500 md:text-5xl">
         {dashboardData[1].title}
       </h1>
       <div className="my-5 md:flex md:justify-between md:flex-row-reverse">
@@ -116,10 +115,10 @@ const Dashboard = () => {
       <div className="border-1 border-gray-400 m-5"></div>
 
       {/* meet the founder of the oganization section  */}
-      <h1 className="mt-10 text-5xl font-heading text-center font-bold text-gray-500 ">
+      <h1 className="my-10 text-3xl font-heading text-center font-bold text-gray-500 md:text-5xl">
         Meet Our Founders
       </h1>
-      <div id="carousal" className="w-[100%] my-10" >
+      <div id="carousal" className="w-[100%] h-2xl my-10">
           <Carausal carousalData={carousalData} />
       </div>
     </>
