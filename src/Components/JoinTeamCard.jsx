1826f7b274b7a607c913296f342855d0968a1a1f
ankush0710 <ankushkurvey053@gmail.com>
2026-03-15 15:58:46 +0530

    data is coming from the api and displayed in cards

---
+++ src/Components/JoinTeamCard.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const JoinTeamCard = ({children}) => {
+    return (
+        <>
+      <div id="gov-carousal" className="w-full h-auto px-3 mb-10 md:flex md:gap-4 overflow-hidden">
+        {children}
+      </div>
+        </>
+    )
+}
+
+export default JoinTeamCard
--- src/Pages/JoinTeam.jsx
+++ src/Pages/JoinTeam.jsx
@@ -1,19 +1,20 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FetchGovProgramData } from "../redux/action/action";
+import JoinTeamCard from "../Components/JoinTeamCard";
 import bgImage from "../assets/Giant-panda.webp";
 
 const JoinTeam = () => {
-    const dispatch = useDispatch();
-    const govProgram = useSelector((state) => state.GovProgram);
+  const dispatch = useDispatch();
+  const govProgram = useSelector((state) => state.GovProgram);
 
   //disppatch the funtion and render the data
-    useEffect(()=>{
-        dispatch(FetchGovProgramData());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(FetchGovProgramData());
+  }, [dispatch]);
   return (
     <>
-    {/* bg -image and intro of page  */}
+      {/* bg -image and intro of page  */}
       <div id="bg-image" className="w-full min-h-screen">
         <div
           id="banner-image"
@@ -28,11 +29,62 @@ const JoinTeam = () => {
           </div>
         </div>
       </div>
+    
+        {/* line for seperation  */}
+      <div className="border-1 border-gray-400 my-5 mx-3"></div>
 
       {/* carausal section for goverment initiatives and Program  */}
-      <div id="gov-carousal">
-        
-      </div>
+      <h1 className="mb-5 text-3xl font-heading text-center font-bold text-gray-500 md:text-5xl">
+        Goverment Initiatives
+      </h1>
+      <JoinTeamCard>
+        {govProgram.map((gp) => {
+          return (
+            <>
+              <div className="min-w-78 h-auto mb-10 md:min-w-xl border-1 border-gray-400 rounded-xl">
+                <div id="crad-image" className="w-full h-75 rounded-t-xl">
+                  <img src={gp.imageUrl} alt="card-image" className="w-full h-full object-cover object-center rounded-t-xl"/>
+                </div>
+                <div className="border-1 border-gray-400 mx-4 "></div>
+                <div id="card-content" className="p-5 space-y-3">
+                  <div className="flex justify-between">
+                    <p className="font-heading font-semibold text-md text-black">
+                      Project Name:{" "}
+                      <span className="font-body text-md text-gray-600">
+                        {gp.programName}
+                      </span>
+                    </p>
+                    <p className="font-heading font-semibold text-md text-black">
+                      Country:{" "}
+                      <span className="font-body text-md text-gray-600">
+                        {gp.country}
+                      </span>
+                    </p>
+                  </div>
+                  <p className="font-heading font-semibold text-md text-black">
+                    Launched Year:{" "}
+                    <span className="font-body text-md text-gray-600">
+                      {gp.year}
+                    </span>
+                  </p>
+                  <p className="font-heading font-semibold text-md text-black">
+                    About:{" "}
+                    <span className="font-body text-md text-gray-600">
+                      {gp.description}
+                    </span>
+                  </p>
+                  <p className="font-heading font-semibold text-md text-black">
+                    Department:{" "}
+                    <span className="font-body text-md text-gray-600">
+                      {gp.goverment}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </JoinTeamCard>
     </>
   );
 };
